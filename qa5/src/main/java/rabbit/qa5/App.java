18134b48454a45371e6bccbda1b4634430720d6c
lewei <lewei@DESKTOP-UVA71NO>
2017-02-22 13:38:42 +0800

    initial project qa5

---
+++ qa5/src/main/java/rabbit/qa5/App.java
@@ -0,0 +1,13 @@
+package rabbit.qa5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
