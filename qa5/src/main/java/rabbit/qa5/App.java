b0a0d6ac3648198bd40ea2b6285447337fa87feb
lewei <lewei@DESKTOP-UVA71NO>
2017-02-22 14:52:22 +0800

    dfasdfa

--- qa5/src/main/java/rabbit/qa5/App.java
+++ qa5/src/main/java/rabbit/qa5/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World  0006!" );
+        
+        System.out.println( "Hello World  0009!" );
     }
 }
